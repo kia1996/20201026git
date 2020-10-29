45718cd8e4b054dab99cde0adea2daf5c4434d38
kia1996 <69577707+kia1996@users.noreply.github.com>
2020-10-30 00:47:31 +0900

    업데이트 2

--- src/aaaggfadfs.java
+++ src/aaaggfadfs.java
@@ -2,6 +2,12 @@
 public class aaaggfadfs {
 	public static void main(String[] args) {
 		System.out.println("653");
+		System.out.println("653");
+		System.out.println("653");
+		System.out.println("653");
+		System.out.println("653");
+		System.out.println("653");
+		System.out.println("653");
 	}
 
 }
